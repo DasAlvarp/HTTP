2c600afaa5835870ec88cdb30f85e5c04e74821d
alvarpq <alvarogudis@gmail.com>
2017-12-01 15:45:20 -0500

    Initial Commit

---
+++ HttpServer.java
@@ -0,0 +1,17 @@
+import java.net.InetSocketAddress;
+
+public class HttpServer
+{
+	public static void main(String[] args)
+	{
+		int port = Integer.parseInt(args[0]);
+		while(running){
+			//recieve message
+			//send response:
+			//-status line
+			//-entity headers
+			//the actual body (use java FileInputStream)
+
+		}
+	}
+}
