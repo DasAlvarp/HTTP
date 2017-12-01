fcb842a226c6072656620b3e7ac7455a199fa1ca
alvarpq <alvarogudis@gmail.com>
2017-12-01 16:05:16 -0500

    Alright, we're getting the first line. This looks good.

--- HttpServer.java
+++ HttpServer.java
@@ -1,17 +1,34 @@
-import java.net.InetSocketAddress;
+import java.io.*;
+import java.net.*;
 
 public class HttpServer
 {
 	public static void main(String[] args)
 	{
+		boolean running = true;
 		int port = Integer.parseInt(args[0]);
-		while(running){
-			//recieve message
-			//send response:
-			//-status line
-			//-entity headers
-			//the actual body (use java FileInputStream)
+		try{
+			//set up reciever, get reciever set up.
+			final ServerSocket server = new ServerSocket(port);
 
+			while(running){
+				Socket client = server.accept();
+				BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				String messageRecieved;
+				//recieve message
+				if(in.ready())
+				{
+					messageRecieved = in.readLine();
+					System.out.println("msg" + messageRecieved);				
+					//send response in a thread. Keep on looping.
+					//-status line
+					//-entity headers
+					//the actual body (use java FileInputStream)
+				}
+
+			}
+		}catch(Exception e){
+			System.out.println("there was an error");
 		}
 	}
 }
